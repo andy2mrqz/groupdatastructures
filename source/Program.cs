bee25224a4de5f142bfed0cbdfe1f84baa5c6c07
andy2mrqz <andy2mgcc@gmail.com>
2017-10-04 16:55:10 -0600

    Add comments to Program.cs

--- source/Program.cs
+++ source/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+/*
+ * Andrew Marquez, Evan McDaniel, Nick Trowbridge, Riley Wells
+ * 
+ * This program will prompt the user to run operations on stacks, queues, or dictionaries.  It prompts which data structure
+ * the user would like to manipulate, then moves onto the ManipulateStructure.cs file to show menus with operations that can be
+ * performed there.  Users can add items, display the contents, remove individual items, or clear the data structure completely.
+ * It also can tell you how fast each data structure searches for a particular item.
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +18,13 @@ namespace GroupCSharp
 {
     class Program
     {
+        //Ensures valid input of integers
         public static int CheckNums(string input)
         {
             int validatedInt = 0;
 
+            /*If the input can be parsed to an integer, it outputs it and returns.
+              If not, it prompts for a number */
             while (!Int32.TryParse(input, out validatedInt))
             {
                 Console.WriteLine("\nPlease enter a number\n");
@@ -23,6 +36,7 @@ namespace GroupCSharp
 
         static void Main(string[] args)
         {
+            //Variable Declarations
             string userInput = "";
             int menuChoice = 0;
             int subChoice = 0;
@@ -33,48 +47,60 @@ namespace GroupCSharp
 
             do
             {
+                //Display the menu
                 Console.WriteLine("1. Stack");
                 Console.WriteLine("2. Queue");
                 Console.WriteLine("3. Dictionary");
                 Console.WriteLine("4. Exit\n");
 
+                //Get the menu choice from the user
                 userInput = Console.ReadLine();
                 menuChoice = CheckNums(userInput);
                 Console.WriteLine();
 
+                //Given the menu choice, a switch will show the menu for each data structure
                 switch (menuChoice)
                 {
-                    case 1:
+                    case 1: //Stack
                         ManipulateStructure handleStack = new ManipulateStructure();
                         subChoice = 0;
+                        //Remains in stack menu until they select to return to the main menu
                         while (subChoice != 7)
                         {
+                            //Retrieves menu choice from the stack menu
                             subChoice = handleStack.Menu("Stack");
-                            handleStack.modifyStack(subChoice, ref myStack);
+                            //Goes to that particular menu item for the stack manipulation
+                            handleStack.modifyStack(subChoice, myStack);
                         }
                         break;
-                    case 2:
+                    case 2: //Queue
                         ManipulateStructure handleQueue = new ManipulateStructure();
                         subChoice = 0;
+                        //Remains in queue menu until they select to return to the main menu
                         while (subChoice != 7)
                         {
+                            //Retrieves menu choice from the queue menu
                             subChoice = handleQueue.Menu("Queue");
-                            handleQueue.modifyQueue(subChoice, ref myQueue);
+                            //Goes to that particular menu item for the queue manipulation
+                            handleQueue.modifyQueue(subChoice, myQueue);
                         }
                         break;
-                    case 3:
+                    case 3: //Dictionary
                         ManipulateStructure handleDictionary = new ManipulateStructure();
                         subChoice = 0;
+                        //Remains in dictionary menu until they select to return to the main menu
                         while (subChoice != 7)
                         {
+                            //Retrieves menu choice from the dictionary menu
                             subChoice = handleDictionary.Menu("Dictionary");
-                            handleDictionary.modifyDictionary(subChoice, ref myDictionary);
+                            //Goes to that particular menu item for the dictionary manipulation
+                             handleDictionary.modifyDictionary(subChoice, myDictionary);
                         }
                         break;
-                    case 4:
+                    case 4: //Exit
                         Console.WriteLine("Goodbye!");
                         break;
-                    default:
+                    default: //Invalid menu number
                         Console.WriteLine("Please choose one of the menu options");
                         break;
                 }
